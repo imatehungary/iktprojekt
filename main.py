bd60c8444ac3061bd8424b285fd59fd7518717c5
imatehungary <84866334+imatehungary@users.noreply.github.com>
2022-06-06 14:32:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Mi a feladat megoldása, ha x2+7x+12=0?")
+x1 = int(input("Kérem x1-et: "))
+x2 = int(input("Kérem x2-őt: "))
+if x1 == -3 and x2 == -4:
+    print("Szép munka!")
+else:
+    print("Nézd meg a weboldalon a megoldást!")
+
+print("Mi a feladat megoldása, ha x2+2x-8")
+x11 = int(input("Kérem x1-et: "))
+x22 = int(input("Kérem x2-őt: "))
+if x11 == +2 and x22 == -4:
+    print("Szép munka!")
+else:
+    print("Nézd meg a weboldalon a megoldást!")
+
+print("Mi a feladat megoldása, ha x4+x2-20")
+x111 = int(input("Kérem x1-et: "))
+x211 = int(input("Kérem x2-őt: "))
+if x111 == +9 and x222 == +1:
+    print("Szép munka!")
+else:
+    print("Nézd meg a weboldalon a megoldást!")
+
+print("Mi a feladat megoldása, ha x2-4-6x+24")
+x1111 = int(input("Kérem x1-et: "))
+x2222 = int(input("Kérem x2-őt: "))
+if x1111 == +6 and x2222 == +4:
+    print("Szép munka!")
+else:
+    print("Nézd meg a weboldalon a megoldást!")

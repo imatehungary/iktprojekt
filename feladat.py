d1d40adb24cc9c7867916ffe0e6b7371098801de
imatehungary <84866334+imatehungary@users.noreply.github.com>
2022-06-06 14:25:15 +0200

    Add files via upload

---
+++ feladat.py
@@ -0,0 +1,7 @@
+print("Mi a feladat megoldása, ha x2+7x+12=0?")
+x1 = int(input("Kérem x1-et: "))
+x2 = int(input("Kérem x2-őt: "))
+if x1 == -3 and x2 == -4:
+    print("Szép munka!")
+else:
+    print("Nézd meg a weboldalon a megoldást!")
